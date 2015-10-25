65850e01dd6443b1ac1c5edd01238fb939df455c
silent-sea1119 <sandragonzales56t@gmail.com>
2015-10-25 00:08:01 -0600

    Create mediasite SDK and start using it

--- src/urls.py
+++ src/urls.py
@@ -4,5 +4,6 @@ from webapp2 import Route, SimpleRoute
 
 ROUTES = [
     Route('/api/v1/songs/get/', handler='app.views.api.v1.songs.SongsApiHandler'),
+    Route('/api/v1/song/get/<song_id:\d+>', handler='app.views.api.v1.songs.SongApiHandler'),
     SimpleRoute('/*', handler='app.views.main.MainView'),
 ]
