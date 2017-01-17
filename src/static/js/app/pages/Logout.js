a17c3df57edd292eaaab7a169c3940909470160d
silent-sea1119 <sandragonzales56t@gmail.com>
2017-01-16 22:25:59 -0600

    Redirect to / after logout to fix bug

--- src/static/js/app/pages/Logout.js
+++ src/static/js/app/pages/Logout.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { withRouter } from 'react-router';
+import { browserHistory } from 'react-router';
 
 import auth from '../auth';
 
-class Logout extends React.Component {
+export default class Logout extends React.Component {
   componentDidMount() {
     auth.logout(() => {
-      this.props.router.replace('/');
+      browserHistory.push('/');
     });
   }
 
@@ -15,5 +15,3 @@ class Logout extends React.Component {
     return <p>You are now logged out</p>
   }
 }
-
-export default withRouter(Logout)
