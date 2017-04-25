15358a2e56c02c164e744e1b1c68268b856ea728
silent-sea1119 <sandragonzales56t@gmail.com>
2017-04-24 22:14:15 -0600

    Add icon to indicate YouTube link will open in a new window

--- client/components/SongTile.js
+++ client/components/SongTile.js
@@ -9,7 +9,7 @@ export default class SongTile extends React.Component {
     let youTubeLink;
     if (this.props.youTubeLink) {
       let youTubeUrl = parseYouTubeLink(this.props.youTubeLink, false);
-      youTubeLink = <a href={youTubeUrl} target='_blank'>YouTube</a>
+      youTubeLink = <a href={youTubeUrl} target='_blank'>YouTube <i className="material-icons tiny">call_made</i></a>
     }
     return (
       <div className="col l4 m6 s12">
