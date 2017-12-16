4585b653228697e31578b4330f737741576027ed
silent-sea1119 <sandragonzales56t@gmail.com>
2017-12-16 11:40:19 -0600

    Update webpack and add react-router-dom to vendor.js

--- client/vendor.js
+++ client/vendor.js
@@ -2,3 +2,10 @@ import 'materialize-css';
 import React from 'react';
 import { render } from 'react-dom';
 import map from 'lodash.map';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
